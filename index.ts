063fec5cf5844973ca3dadaa96eecb09e7014b2a
Cesar Gomez <cesaregomezg0@gmail.com>
2023-08-30 14:42:13 -0600

    Ajustes de CORS

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 
 import ColorsController from './controllers/colors'
@@ -7,6 +8,18 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.includes(origin) || origin === undefined) {
+      callback(null, true)
+    } else {
+      callback(new Error('Unauthorized'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 app.use('/colors', ColorsController)
 
